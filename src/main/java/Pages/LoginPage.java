bfa86ecfe6969feda71c62b6f1cc446b489cba62
chinthaka-rex <cwithanage.c@rex.com>
2022-01-26 12:20:03 +0530

    test on browserstack

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -2,10 +2,7 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.concurrent.TimeUnit;
 
 public class LoginPage {
     private WebDriver driver;
