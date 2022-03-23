2a0c1344c0ebda5eb32f2c30f1ec2299fae35e3f
chinthakaw <chinthaka.wit@gmail.com>
2022-03-23 21:38:24 +0530

    Build Your Plan Page Impl

--- src/test/java/MyTestRunner/TestRunner.java
+++ src/test/java/MyTestRunner/TestRunner.java
@@ -20,7 +20,7 @@ import java.util.Properties;
         monochrome = false,
         glue = {"Steps", "AppHooks"},
         //tags = "@Smoke",
-        features = {"src/test/resources/Features/ContactInformationPage.feature"}
+        features = {"src/test/resources/Features/BuildYourPlanPage.feature"}
 )
 
 public class TestRunner extends AbstractTestNGCucumberTests {
